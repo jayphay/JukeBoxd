23e078a41854bb68df8b6a0d50fdf6d3c5c4f236
Jack Phalen <jbp54799@uga.edu>
2026-04-20 18:26:52 -0400

    inital commit

---
+++ src/main/java/uga/group18/app/App.java
@@ -0,0 +1,17 @@
+package uga.group18.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * This is the application entry point. Do not change this unless you know
+ * what you are doing.
+ */
+@SpringBootApplication
+public class App
+{
+    public static void main(String[] args )
+    {
+        SpringApplication.run(App.class, args);
+    }
+}
---
+++ src/test/java/uga/group18/app/AppTests.java
@@ -0,0 +1,13 @@
+package uga.group18.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
