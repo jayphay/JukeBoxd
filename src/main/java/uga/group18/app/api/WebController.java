9a2e98757192b571a53356aa68e2b1ccbef1dbcb
shachinc <shachinc393@gmail.com>
2026-05-02 16:41:55 -0400

    Top bar

---
+++ src/main/java/uga/group18/app/api/WebController.java
@@ -0,0 +1,29 @@
+package uga.group18.app.api;
+
+import org.springframework.jdbc.core.JdbcOperations;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+
+import java.util.List;
+
+@Controller
+public class WebController {
+
+    private final JdbcTemplate jdbc;
+
+    public WebController(JdbcTemplate jdbc) {
+        this.jdbc = jdbc;
+    }
+
+    @GetMapping("/")
+    public String home() {
+        return "index";
+    }
+
+    @ModelAttribute("genres")
+    public List<String> getGenres() {
+        return jdbc.queryForList("SELECT DISTINCT genre FROM song WHERE genre IS NOT NULL", String.class);
+    }
+}
